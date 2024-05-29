e93dd1eb89e672754476d1c9789b950603ff12a3
pedriinn11 <henriqueopedro11@gmail.com>
2024-05-29 02:44:55 -0300

    tudo rodando!!

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,8 +33,58 @@ function updateSoftSkills(profileData) {
     softSkills.innerHTML = profileData.skills.softSkills.map(skill => `<li>${skill}</li>`).join('')
 }
 
+function updateHardSkills(profileData) {
+    const hardSkills = document.getElementById('profile.skills.hardSkills')
+    hardSkills.innerHTML = profileData.skills.hardSkills.map(skill => `<li><img src="${skill.logo}" alt="${skill.name}" title="${skill.name}"></li>`).join('')
+}
+
+function updateLanguages(profileData) {
+    const languages = document.getElementById('profile.languages')
+    languages.innerHTML = profileData.languages.map(language => `<li>${language}</li>`).join('')
+}
+
+function updatePortfolio(profileData) {
+    const portfolio = document.getElementById('profile.portfolio')
+    portfolio.innerHTML = profileData.portfolio.map(project => {
+        return `
+            <li>
+                <h3 ${project.github ? 'class="github"' : ''}>${project.name}</h3>
+                <a href="${project.url}" target="_blank">${project.url}</a>
+            </li>
+        `
+    }).join('')
+}
+
+
+function updateConquest(profileData) {
+    const conquest = document.getElementById('profile.conquest')
+    conquest.innerHTML = profileData.conquest.map(conquest => `<li><img src="${conquest.logo}" alt="${conquest.name}" title="${conquest.name}"></li>`).join('')
+}
+
+function updateEducation(profileData) {
+    console.log(profileData)
+    const education = document.getElementById('profile.education')
+    education.innerHTML = profileData.education.map(education => {
+        return `
+            <li>
+               <h3>${education.name}</h3>
+               <p class="period">${education.period}</p>
+               <p>${education.description}</p>
+
+            </li>
+        `
+    }).join('')
+}
+
+
 (async () => {
-    const profileData = await fetchProfileData()
-    updateProfileInfo(profileData)
-    updateSoftSkills(profileData)
+    const profileData = await fetchProfileData();
+    updateProfileInfo(profileData);
+    updateSoftSkills(profileData);
+    updateHardSkills(profileData);
+    updateLanguages(profileData);
+    updatePortfolio(profileData);
+    updateEducation(profileData)
+    updateConquest(profileData)
+
 })()
